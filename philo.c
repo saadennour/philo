edd13bc63854886341e5731846a5354832b170e5
Saad-ennour Farhan <sfarhan@e2r9p11.1337.ma>
2022-06-06 23:38:06 +0100

    forks

--- philo.c
+++ philo.c
@@ -6,7 +6,7 @@
 /*   By: sfarhan <sfarhan@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/21 17:44:58 by sfarhan           #+#    #+#             */
-/*   Updated: 2022/05/31 21:03:10 by sfarhan          ###   ########.fr       */
+/*   Updated: 2022/06/06 23:33:25 by sfarhan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 pthread_mutex_t mutex;
 
-void	*philo_born()
+void	*philo_born(void *guests)
 {
 	static int i = 1;
 	
@@ -25,32 +25,46 @@ void	*philo_born()
 	return (0);
 }
 
+t_philo doc_strange(int ac, char **av)
+{
+	t_philo rules;
+	
+	rules.die = ft_atoi(av[2]);
+	rules.eat = ft_atoi(av[3]);
+	rules.sleep = ft_atoi(av[4]);
+	if (ac == 6)
+		rules.meals = ft_atoi(av[5]);
+	return (rules);
+}
+
 int main(int ac, char **av)
 {
 	pthread_t	*thread_id;
-	int num_philo;
-	int	i;
+	t_philo		*rules = NULL;
+	int 		num_philo;
+	int			i;
 
-	i = 1;
+	i = 0;
 	thread_id = NULL;
 	if (ac < 5)
 		printf ("Error\n");
 	else
 	{
 		num_philo = ft_atoi(av[1]);
+		rules = malloc(sizeof(t_philo) * num_philo);
+		while (i++ < num_philo - 1)
+			rules[i] = doc_strange(ac, av);
 		thread_id = malloc(sizeof(pthread_t) * num_philo);
 		pthread_mutex_init(&mutex, NULL);
-		while (i <= num_philo)
+		i = 0;
+		while (i++ < num_philo)
 		{
-			pthread_create(&thread_id[i], NULL, &philo_born, NULL);
-			i++;
+			printf ("%d\n", i);
+			pthread_create(&thread_id[i], NULL, &philo_born, &rules);
 		}
-		i = 1;
-		while (i <= num_philo)
-		{
+		i = 0;
+		while (i++ < num_philo)
 			pthread_join(thread_id[i], NULL);
-			i++;
-		}
 	}
 	pthread_mutex_destroy(&mutex);
 	free(thread_id);
--- philo.h
+++ philo.h
@@ -6,7 +6,7 @@
 /*   By: sfarhan <sfarhan@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/21 17:44:23 by sfarhan           #+#    #+#             */
-/*   Updated: 2022/05/30 17:11:19 by sfarhan          ###   ########.fr       */
+/*   Updated: 2022/06/06 22:58:38 by sfarhan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,16 @@
 #include <pthread.h>
 #include <stdio.h>
 #include <stdlib.h>
+
+typedef struct s_philo
+{
+	int	die;
+	int eat;
+	int	sleep;
+	int	meals;
+} t_philo ; 
+
+
 
 long long	ft_atoi(const char *str);
 
